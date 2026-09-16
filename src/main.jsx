242cc54595cb8f413573e4d08d29349e6a42a694
bulbul <bulbulpachauri21@gmail.com>
2026-09-17 01:21:06 +0530

    Update sitemap with product page

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { CartProvider } from "./context/CartContext.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
-        <App />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
       </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
